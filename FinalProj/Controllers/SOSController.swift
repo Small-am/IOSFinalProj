c0158d0f7fb9a1454503210d71a7d6229936bdd4
Kevin <lykevi@sheridancollege.ca>
2021-12-12 17:08:26 -0800

    Clock and SOS created

--- FinalProj/Controllers/SOSController.swift
+++ FinalProj/Controllers/SOSController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class SOSController: UIViewController {
 
+    
+
+    @IBAction func sosButton(_ sender: Any) {
+        let formattedNumber = phoneNumberVariable.components(separatedBy: NSCharacterSet.decimalDigits.inverted).joined(separator: "")
+
+        if let url = NSURL(string: ("tel:" + (formattedNumber)!)) {
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)
+            } else {
+                UIApplication.shared.openURL(url as URL)
+            }
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
